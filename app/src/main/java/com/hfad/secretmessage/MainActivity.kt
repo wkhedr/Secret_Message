7c8c18ef0c8d77623fd61d9d4403f03cd7a3e2e0
wkhedr <khedrw@gmail.com>
2022-11-13 14:14:05 +0200

    Initial commit

---
+++ app/src/main/java/com/hfad/secretmessage/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hfad.secretmessage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
